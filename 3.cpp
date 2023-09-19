f6f04965f8f62af9c8274c22c79e75e22761425d
Fadhilkhan04 <114307570+Fadhilkhan04@users.noreply.github.com>
2023-09-19 20:37:37 +0530

    changes

--- 3.cpp
+++ 3.cpp
@@ -1,72 +1,115 @@
 #include <iostream>
 using namespace std;
 
-void deleted(int mynum[], int n, int x)
+void display(int mynum[], int i, int n)
+{
+  for (i = 0; i < n; i++)
+  {
+    cout << mynum[i] << " ";
+  }
+}
+void insert(int mynum[], int n, int num, int pos)
 {
   int i;
-  for (i = 0; i < n + 1; i++)
+  n++;
+  for (i = n; i >= pos; i--)
   {
-    if (mynum[i] == x)
+    mynum[i] = mynum[i - 1];
+  }
+  mynum[pos - 1] = num;
+}
+
+void Delete(int mynum[], int n, int x)
+{
+  int i;
+  for (i = 0; i < n; i++)
+  {
+    if (i + 1 == x)
     {
       break;
     }
   }
-  if (i < n + 1)
+  if (i < n)
   {
     for (int j = i; j < n; j++)
     {
       mynum[j] = mynum[j + 1];
     }
   }
-  cout << "current array is" << endl;
-  for (i = 0; i < n; i++)
-    cout << mynum[i] << " ";
-  cout << endl;
-}
-
-void insert(int mynum[], int n, int num, int pos)
-{
-  int i;
-  for (i = n + 1; i >= pos; i--)
-  {
-    mynum[i] = mynum[i - 1];
-  }
-  mynum[pos - 1] = num;
-  cout << "current array is" << endl;
-  for (i = 0; i < n + 1; i++)
-    cout << mynum[i] << " ";
-  cout << endl;
 }
 
 int main()
 {
-  int n, i, num, pos, x;
+  int n, i, size, ch, num, pos, x;
   cout << "Enter the size of the array : ";
+  cin >> size;
+  cout << "Enter the number of elements in the array : ";
   cin >> n;
-  int mynum[n + 1];
-  cout << "Enter the elements of the array : ";
+  int mynum[size];
+  cout << "Enter the elements of the array : " << endl;
   for (i = 0; i < n; i++)
+  {
     cin >> mynum[i];
-  cout << "current array is" << endl;
-  for (i = 0; i < n; i++)
-    cout << mynum[i] << " ";
-  cout << endl;
+  }
 
-  cout << "enter the number you want to insert :" << endl;
-  cin >> num;
+  while (true)
+  {
+    cout << endl;
+    cout << "----------- Array Operation -----------" << endl;
+    cout << "1. Display Array." << endl;
+    cout << "2. Insert Array." << endl;
+    cout << "3. Delete Array." << endl;
+    cout << "4. Exit" << endl;
+    cout << "Enter choice: ";
 
-  cout << "enter the position you want to insert in :" << endl;
-  cin >> pos;
+    cin >> ch;
 
-  insert(mynum, n, num, pos);
+    if (ch == 1)
+    {
 
-  cout << "enter the element you want to delete :" << endl;
-  cin >> x;
+      display(mynum, i, n);
+      continue;
+    }
+    else if (ch == 2)
+    {
 
-  deleted(mynum, n, x);
+      cout << endl
+           << "Enter element to insert: ";
+      cin >> num;
 
-  cout << "fadhilkhan" << endl;
-  cout << "220071601060" << endl;
+      cout << "Enter the position you want to insert in: ";
+      cin >> pos;
+      cout << endl;
 
-  return 0;
+      insert(mynum, n, num, pos);
+      n++;
+      continue;
+    }
+    else if (ch == 3)
+    {
+
+      cout << endl
+           << "Enter position you want to delete in: ";
+      cin >> x;
+
+      Delete(mynum, n, x);
+      n--;
+      cout << endl;
+      continue;
+    }
+    else if (ch == 4)
+    {
+      break;
+    }
+    else
+    {
+
+      cout << endl
+           << "Invalid choice" << endl
+           << endl;
+      continue;
+    }
+
+    return 0;
+  }
 }
